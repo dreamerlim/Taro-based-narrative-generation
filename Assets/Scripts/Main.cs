cc681dd2df8c83381efdd44cf9fc67b66899f62b
Seungyun Lee <sylee714@gmail.com>
2021-09-10 23:43:09 -0700

    Move images to Resource folder and found out how to parse json files

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
+using System.IO;
 
+// how to use SimpleJSON
+// Original Implementation
+// https://github.com/annetropy/tarot-narrative/blob/master/tarot/code/tarot.js 
 public class Main : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // var jsonFile = Resources.Load<TextAsset>("json_files/taro-story");
+        string jsonString = File.ReadAllText("json_files/sample.json");
+        var N = JSON.Parse(jsonString);
+        var val = N["version"].Value;
+        // Debug.Log(N["tarot_interpretations"][0]["name"].Value);
+        Debug.Log(val);
     }
 
     // Update is called once per frame
